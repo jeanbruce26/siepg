6e1a60c772f82512ba54a8d38988e2fca4b86f02
Jamt Mendoza <43344540+pupilojamt@users.noreply.github.com>
2023-03-07 09:56:40 -0500

    INSTALACIONDE PAQUETES

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,24 @@
 import './bootstrap';
+
+// Inicializa NProgress
+NProgress.configure({
+    showSpinner: false, // Oculta el spinner
+    trickleSpeed: 500, // Velocidad del efecto de carga
+    ease: 'ease', // Efecto de transición
+    speed: 500, // Velocidad de la transición
+    minimum: 0.1, // Mínimo de la barra de carga
+    trickle: true, // Efecto de carga
+    trickleRate: 0.1, // Velocidad del efecto de carga
+});
+
+// Muestra la barra de carga en cada cambio de página
+window.addEventListener('beforeunload', function() {
+    NProgress.start();
+    NProgress.set(0.4);
+    NProgress.inc();
+});
+
+// Oculta la barra de carga después de que la página se ha cargado completamente
+window.addEventListener('load', function() {
+    NProgress.done();
+});
