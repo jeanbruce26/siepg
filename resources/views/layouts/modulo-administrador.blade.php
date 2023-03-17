04affac28e288ba8433bd2085eb814768d8b4571
Jamt Mendoza <43344540+pupilojamt@users.noreply.github.com>
2023-03-17 11:03:28 -0500

    UPDATE LAYOUT VIEW ADMINISTRADOR

--- resources/views/layouts/modulo-administrador.blade.php
+++ resources/views/layouts/modulo-administrador.blade.php
@@ -1,16 +1,13 @@
 <!DOCTYPE html>
 <html lang="es">
-	
+
 <!--begin::Head-->
 <head>
-	<base href="../" />
 	<title>
         {{ config('app.name', 'Escuela de Posgrado - UNU') }}
     </title>
     <meta charset="utf-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1"/>
-    <meta property="og:locale" content="en_US" />
-    <meta property="og:type" content="article" />
     <link rel="canonical" href="https://preview.keenthemes.com/metronic8"/>
     <link rel="shortcut icon" href="{{ asset('assets/media/logos/logo-pg.png') }}"/>
     <!--begin::Fonts(mandatory for all pages)-->
@@ -33,20 +30,14 @@
 <!--end::Head-->
 <!--begin::Body-->
 
-<body id="kt_app_body" data-kt-app-layout="light-sidebar" data-kt-app-header-fixed="true"
-	data-kt-app-sidebar-enabled="true" data-kt-app-sidebar-fixed="true" data-kt-app-sidebar-hoverable="true"
-	data-kt-app-sidebar-push-header="true" data-kt-app-sidebar-push-toolbar="true"
-	data-kt-app-sidebar-push-footer="true" data-kt-app-toolbar-enabled="true" class="app-default">
-	<!--begin::App-->
+<body id="kt_app_body" data-kt-app-layout="light-sidebar" data-kt-app-header-fixed="true" data-kt-app-sidebar-enabled="true" data-kt-app-sidebar-fixed="true" data-kt-app-sidebar-hoverable="true" data-kt-app-sidebar-push-header="true" data-kt-app-sidebar-push-toolbar="true" data-kt-app-sidebar-push-footer="true" data-kt-app-toolbar-enabled="true" class="app-default">
+	<script>
+        var defaultThemeMode = "light"; var themeMode; if ( document.documentElement ) { if ( document.documentElement.hasAttribute("data-bs-theme-mode")) { themeMode = document.documentElement.getAttribute("data-bs-theme-mode"); } else { if ( localStorage.getItem("data-bs-theme") !== null ) { themeMode = localStorage.getItem("data-bs-theme"); } else { themeMode = defaultThemeMode; } } if (themeMode === "system") { themeMode = window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light"; } document.documentElement.setAttribute("data-bs-theme", themeMode); }
+    </script>
 	<div class="d-flex flex-column flex-root app-root" id="kt_app_root">
-		<!--begin::Page-->
 		<div class="app-page flex-column flex-column-fluid" id="kt_app_page">
-			<!--begin::Header-->
 			<div id="kt_app_header" class="app-header">
-				<!--begin::Header container-->
-				<div class="app-container container-fluid d-flex align-items-stretch justify-content-between"
-					id="kt_app_header_container">
-					<!--begin::Sidebar mobile toggle-->
+				<div class="app-container container-fluid d-flex align-items-stretch justify-content-between" id="kt_app_header_container">
 					<div class="d-flex align-items-center d-lg-none ms-n3 me-1 me-md-2" title="Show sidebar menu">
 						<div class="btn btn-icon btn-active-color-primary w-35px h-35px"
 							id="kt_app_sidebar_mobile_toggle">
@@ -65,29 +56,27 @@
 							<!--end::Svg Icon-->
 						</div>
 					</div>
-					<!--end::Sidebar mobile toggle-->
-					<!--begin::Mobile logo-->
 					<div class="d-flex align-items-center flex-grow-1 flex-lg-grow-0">
 						<a href="#" class="d-lg-none">
 							<img alt="Logo" src="{{ asset('assets/media/logos/logo-pg.png') }}" class="h-30px" />
 						</a>
 					</div>
-					<!-- <div class="d-flex align-items-center flex-grow-1 flex-lg-grow-0">
-						<a href="#" class="d-lg-none">
-							<img alt="Logo" src="{{ asset('assets/media/logos/logo-pg.png') }}" class="h-30px" />
-						</a>
-					</div> -->
-					<!--end::Mobile logo-->
-					<!--begin::Header wrapper-->
 					<div class="d-flex align-items-stretch justify-content-between flex-lg-grow-1"
 						id="kt_app_header_wrapper">
-						<!--begin::Menu wrapper-->
 						<div class="app-header-menu app-header-mobile-drawer align-items-stretch" data-kt-drawer="true"
 							data-kt-drawer-name="app-header-menu" data-kt-drawer-activate="{default: true, lg: false}"
 							data-kt-drawer-overlay="true" data-kt-drawer-width="250px" data-kt-drawer-direction="end"
 							data-kt-drawer-toggle="#kt_app_header_menu_toggle" data-kt-swapper="true"
 							data-kt-swapper-mode="{default: 'append', lg: 'prepend'}"
 							data-kt-swapper-parent="{default: '#kt_app_body', lg: '#kt_app_header_wrapper'}">
+                            <div class="menu menu-rounded menu-column menu-lg-row my-5 my-lg-0 align-items-stretch fw-semibold px-2 px-lg-0" id="kt_app_header_menu" data-kt-menu="true">
+                                <div class="menu-item">
+                                    @php $admision = App\Models\Admision::where('estado', 1)->first()->admision; @endphp
+                                    <span class="fs-3 fw-bold">
+                                        PROCESO DE {{ $admision }}
+                                    </span>
+                                </div>
+                            </div>
 						</div>
 						<!--end::Menu wrapper-->
 						<!--begin::Navbar-->
@@ -605,7 +594,7 @@
 
 	<!--begin::Javascript-->
 	<script> var hostUrl = "assets/"; </script>
-		
+
 	<!--begin::Global Javascript Bundle(mandatory for all pages)-->
 	<script src="{{ asset('assets/plugins/global/plugins.bundle.js') }}"></script>
 	<script src="{{ asset('assets/js/scripts.bundle.js') }}"></script>
@@ -636,7 +625,7 @@
 	<script src="{{ asset('assets/js/custom/utilities/modals/new-target.js') }}"></script>
 	<script src="{{ asset('assets/js/custom/utilities/modals/users-search.js') }}"></script>
 	<!--end::Custom Javascript-->
-	
+
 	<!--begin::Turbolink-->
 	<script type="module">
 	import hotwiredTurbo from 'https://cdn.skypack.dev/@hotwired/turbo';
