f5528e99be9b4d6a2498286fe26cd8a1e3dd2ef5
Jamt Mendoza <43344540+pupilojamt@users.noreply.github.com>
2023-05-23 20:18:12 -0500

    MEJORA DE INTERFACES EN LA VISTA DE LOS COORDINADORES DE UNIDAD

--- resources/views/livewire/modulo-coordinador/inicio/evaluaciones/inscripciones.blade.php
+++ resources/views/livewire/modulo-coordinador/inicio/evaluaciones/inscripciones.blade.php
@@ -41,16 +41,16 @@
                 <div class="d-flex align-items-center text-center gap-2 gap-lg-3 ms-5">
                     @if ($inscripciones->count() == $evaluaciones->count())
                         @if ($programa->programa_tipo == 1)
-                            <a href="{{ route('coordinador.reporte-maestria', ['id_programa' => $id_programa, 'id_admision' => $id_admision]) }}" target="_blank" class="btn btn-info btn-sm">
+                            <a href="{{ route('coordinador.reporte-maestria', ['id_programa' => $id_programa, 'id_admision' => $id_admision]) }}" target="_blank" class="btn btn-info fw-bold">
                                 Generar Acta de Evaluación
                             </a>
                         @else
-                            <a href="{{ route('coordinador.reporte-doctorado', ['id_programa' => $id_programa, 'id_admision' => $id_admision]) }}" target="_blank" class="btn btn-info btn-sm">
+                            <a href="{{ route('coordinador.reporte-doctorado', ['id_programa' => $id_programa, 'id_admision' => $id_admision]) }}" target="_blank" class="btn btn-info fw-bold">
                                 Generar Acta de Evaluación
                             </a>
                         @endif
                     @else
-                        <button type="button" class="btn btn-info btn-sm" disabled>
+                        <button type="button" class="btn btn-info fw-bold" disabled>
                             Generar Acta de Evaluación
                         </button>
                     @endif
