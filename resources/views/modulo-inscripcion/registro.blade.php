4b2a69de5d3acb67f570d8b54d0ccabfa663342a
Jamt Mendoza <43344540+iamjamtt@users.noreply.github.com>
2024-02-28 17:47:28 -0500

    Actualizar vista de registro de inscripción

--- resources/views/modulo-inscripcion/registro.blade.php
+++ resources/views/modulo-inscripcion/registro.blade.php
@@ -1,55 +1,55 @@
 @extends('layouts.modulo-inscripcion')
 @section('content')
-<div class="d-flex flex-column flex-column-fluid">
-    <div id="kt_app_content" class="app-content flex-column-fluid">
-        <div id="kt_app_content_container" class="app-container container-xxl py-8">
-            {{-- @livewire('modulo-inscripcion.registro', [
+    <div class="d-flex flex-column flex-column-fluid">
+        <div id="kt_app_content" class="app-content flex-column-fluid">
+            <div id="kt_app_content_container" class="app-container container-xxl py-8">
+                {{-- @livewire('modulo-inscripcion.registro', [
                 'id_inscripcion' => $id_inscripcion
             ]) --}}
-            @livewire('modulo-inscripcion.registro')
+                @livewire('modulo-inscripcion.registro')
+            </div>
         </div>
     </div>
-</div>
 @endsection
 @section('scripts')
-<script>
-    window.addEventListener('modal_registro_expediente', event => {
-        $('#modal_registro_expediente').modal(event.detail.action);
-    })
-    window.addEventListener('registro_inscripcion', event => {
-        Swal.fire({
-            title: event.detail.title,
-            text: event.detail.text,
-            icon: event.detail.icon,
-            buttonsStyling: false,
-            confirmButtonText: event.detail.confirmButtonText,
-            customClass: {
-                confirmButton: "btn btn-"+event.detail.color,
-            }
-        });
-    })
-    window.addEventListener('alerta_final_registro', event => {
-        let timerInterval;
-        Swal.fire({
-            title: 'Espere un momento, estamos generando su ficha de inscripción',
-            showConfirmButton: false,
-            timer: 30000,
-            timerProgressBar: true,
-            allowOutsideClick: false,
-            allowEscapeKey: false,
-            allowEnterKey: false,
-            padding: '2em 2em 3em 2em',
-            didOpen: () => {
-            Swal.showLoading()
-            const b = Swal.getHtmlContainer().querySelector('b')
-            timerInterval = setInterval(() => {
-                b.textContent = Swal.getTimerLeft()
-            }, 100)
-            },
-            willClose: () => {
-            clearInterval(timerInterval)
-            }
+    <script>
+        window.addEventListener('modal_registro_expediente', event => {
+            $('#modal_registro_expediente').modal(event.detail.action);
         })
-    });
-</script>
+        window.addEventListener('registro_inscripcion', event => {
+            Swal.fire({
+                title: event.detail.title,
+                text: event.detail.text,
+                icon: event.detail.icon,
+                buttonsStyling: false,
+                confirmButtonText: event.detail.confirmButtonText,
+                customClass: {
+                    confirmButton: "btn btn-" + event.detail.color,
+                }
+            });
+        })
+        window.addEventListener('alerta_final_registro', event => {
+            let timerInterval;
+            Swal.fire({
+                title: 'Espere un momento, estamos generando su ficha de inscripción',
+                showConfirmButton: false,
+                timer: 60000,
+                timerProgressBar: true,
+                allowOutsideClick: false,
+                allowEscapeKey: false,
+                allowEnterKey: false,
+                padding: '2em 2em 3em 2em',
+                didOpen: () => {
+                    Swal.showLoading()
+                    const b = Swal.getHtmlContainer().querySelector('b')
+                    timerInterval = setInterval(() => {
+                        b.textContent = Swal.getTimerLeft()
+                    }, 100)
+                },
+                willClose: () => {
+                    clearInterval(timerInterval)
+                }
+            })
+        });
+    </script>
 @endsection
