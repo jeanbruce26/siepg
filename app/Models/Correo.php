4ba35dd814b898badaddbd21b1cf9cae2b6655d1
Jamt Mendoza <43344540+iamjamtt@users.noreply.github.com>
2024-03-09 10:31:10 -0500

    Creación del Modelo Correo

---
+++ app/Models/Correo.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Correo extends Model
+{
+    use HasFactory;
+
+    protected $table = 'correo';
+    protected $primaryKey = 'id_correo';
+    protected $fillable = [
+        'correo_asunto',
+        'correo_mensaje',
+        'correo_enviados',
+        'correo_estado'
+    ];
+
+    public function getEnviadosAttribute()
+    {
+        ///...
+    }
+}
