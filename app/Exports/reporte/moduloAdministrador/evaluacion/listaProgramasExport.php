71671e0e1bd0c86951a09d06475c7b3f65eb7321
Jamt Mendoza <43344540+iamjamtt@users.noreply.github.com>
2024-04-17 23:06:50 -0500

    Corregir condición de búsqueda en listaProgramasExport.php

--- app/Exports/reporte/moduloAdministrador/evaluacion/listaProgramasExport.php
+++ app/Exports/reporte/moduloAdministrador/evaluacion/listaProgramasExport.php
@@ -16,16 +16,28 @@ class listaProgramasExport implements WithMultipleSheets
     {
         $sheets = [];
 
-        $programas = Inscripcion::join('programa_proceso','inscripcion.id_programa_proceso','=','programa_proceso.id_programa_proceso')
-            ->join('programa_plan','programa_proceso.id_programa_plan','=','programa_plan.id_programa_plan')
-            ->join('programa','programa_plan.id_programa','=','programa.id_programa')
-            ->where('programa.programa_estado',1)
-            ->where('programa.id_modalidad',2)
-            ->where('inscripcion.inscripcion_estado',1)
-            ->where('inscripcion.retiro_inscripcion',0)
-            ->where('inscripcion.verificar_expedientes',1)
-            ->groupBy('inscripcion.id_programa_proceso')
-            ->select('programa.id_programa','programa.programa','programa.subprograma','programa.mencion')
+        // $programas = Inscripcion::join('programa_proceso','inscripcion.id_programa_proceso','=','programa_proceso.id_programa_proceso')
+        //     ->join('programa_plan','programa_proceso.id_programa_plan','=','programa_plan.id_programa_plan')
+        //     ->join('programa','programa_plan.id_programa','=','programa.id_programa')
+        //     ->where('programa.programa_estado',1)
+        //     ->where('programa.id_modalidad',2)
+        //     ->where('inscripcion.inscripcion_estado',1)
+        //     ->where('inscripcion.retiro_inscripcion',0)
+        //     ->where('inscripcion.verificar_expedientes',1)
+        //     ->groupBy('inscripcion.id_programa_proceso')
+        //     ->select('programa.id_programa','programa.programa','programa.subprograma','programa.mencion')
+        //     ->get();
+
+        $programas = Inscripcion::join('programa_proceso', 'inscripcion.id_programa_proceso', '=', 'programa_proceso.id_programa_proceso')
+            ->join('programa_plan', 'programa_proceso.id_programa_plan', '=', 'programa_plan.id_programa_plan')
+            ->join('programa', 'programa_plan.id_programa', '=', 'programa.id_programa')
+            ->where('programa.programa_estado', 1)
+            ->where('programa.id_modalidad', 2)
+            ->where('inscripcion.inscripcion_estado', 1)
+            ->where('inscripcion.retiro_inscripcion', 0)
+            ->where('inscripcion.verificar_expedientes', 1)
+            ->distinct()
+            ->select('programa.id_programa', 'programa.programa', 'programa.subprograma', 'programa.mencion')
             ->get();
 
         foreach ($programas as $programa) {
