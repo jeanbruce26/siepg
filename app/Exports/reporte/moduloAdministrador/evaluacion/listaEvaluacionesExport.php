72a09b46e7f6b3d0ab3117bccb931536fa9d3c69
Jamt Mendoza <43344540+iamjamtt@users.noreply.github.com>
2024-04-23 22:36:51 -0500

    Corregir formato de exportación en listaEvaluacionesExport.php y listaProgramasExport.php

--- app/Exports/reporte/moduloAdministrador/evaluacion/listaEvaluacionesExport.php
+++ app/Exports/reporte/moduloAdministrador/evaluacion/listaEvaluacionesExport.php
@@ -2,6 +2,7 @@
 
 namespace App\Exports\reporte\moduloAdministrador\evaluacion;
 
+use App\Models\Evaluacion;
 use App\Models\Inscripcion;
 use App\Models\ProgramaProceso;
 use Maatwebsite\Excel\Events\AfterSheet;
@@ -21,6 +22,7 @@ class listaEvaluacionesExport implements FromCollection, WithMapping, ShouldAuto
 
     protected $item = 0;
     protected $programa;
+    protected $tipo_programa;
     protected $programa_nombre;
     protected $programa_nombre_corto;
 
@@ -37,6 +39,7 @@ class listaEvaluacionesExport implements FromCollection, WithMapping, ShouldAuto
             $this->programa_nombre_corto = $this->programa->programa_plan->programa->programa . ' EN ' . $this->programa->programa_plan->programa->subprograma;
             $this->programa_nombre_corto = Str::slug($this->programa_nombre_corto, ' ');
         }
+        $this->tipo_programa = $this->programa->programa_plan->programa->programa_tipo;
     }
 
     public function collection()
@@ -64,14 +67,40 @@ class listaEvaluacionesExport implements FromCollection, WithMapping, ShouldAuto
         } else {
             $programa = $inscritos->programa . ' EN ' . $inscritos->subprograma;
         }
-        return [
-            $this->item++,
-            $inscritos->numero_documento,
-            $inscritos->apellido_paterno . ' ' . $inscritos->apellido_materno,
-            $inscritos->nombre,
-            $inscritos->celular,
-            $programa
-        ];
+        $evaluacion = Evaluacion::where('id_inscripcion', $inscritos->id_inscripcion)->first();
+        $puntaje_expediente = $evaluacion ? ( $evaluacion->puntaje_expediente ? $evaluacion->puntaje_expediente : '-' ) : '-';
+        $puntaje_investigacion = $evaluacion ? ( $evaluacion->puntaje_investigacion ? $evaluacion->puntaje_investigacion : '-' ) : '-';
+        $puntaje_entrevista = $evaluacion ? ( $evaluacion->puntaje_entrevista ? $evaluacion->puntaje_entrevista : '-' ) : '-';
+        $puntaje_final = $evaluacion ? ( $evaluacion->puntaje_final ? $evaluacion->puntaje_final : '-' ) : '-';
+
+        if ($this->tipo_programa == 1) {
+            return [
+                $this->item++,
+                $inscritos->numero_documento,
+                $inscritos->apellido_paterno . ' ' . $inscritos->apellido_materno,
+                $inscritos->nombre,
+                $inscritos->celular,
+                $inscritos->correo,
+                $puntaje_expediente,
+                $puntaje_entrevista,
+                $puntaje_final,
+                $programa
+            ];
+        } else {
+            return [
+                $this->item++,
+                $inscritos->numero_documento,
+                $inscritos->apellido_paterno . ' ' . $inscritos->apellido_materno,
+                $inscritos->nombre,
+                $inscritos->celular,
+                $inscritos->correo,
+                $puntaje_expediente,
+                $puntaje_investigacion,
+                $puntaje_entrevista,
+                $puntaje_final,
+                $programa
+            ];
+        }
     }
 
     public function registerEvents(): array
@@ -121,22 +150,45 @@ class listaEvaluacionesExport implements FromCollection, WithMapping, ShouldAuto
                 ];
 
                 $event->sheet->setCellValue('A1', 'LISTADO DE INSCRITOS PARA LAS EVALUACIONES - ' . $this->programa_nombre);
-                $event->sheet->getDelegate()->getStyle('A1:F1')->applyFromArray($tamanio);
-                $event->sheet->getDelegate()->getStyle('A1:F1')->applyFromArray($negrita);
-                $event->sheet->getDelegate()->mergeCells('A1:F1');
-                $event->sheet->getDelegate()->getStyle('A1:F1')->applyFromArray($centrar);
 
-                $columnas = ['N°', 'DNI', 'APELLIDOS', 'NOMBRES', 'CELULAR', 'PROGRAMA ACADEMICO'];
-                $event->sheet->getDelegate()->fromArray($columnas, NULL, 'A3');
+                if ($this->tipo_programa == 1) {
+                    $event->sheet->getDelegate()->getStyle('A1:J1')->applyFromArray($tamanio);
+                    $event->sheet->getDelegate()->getStyle('A1:J1')->applyFromArray($negrita);
+                    $event->sheet->getDelegate()->mergeCells('A1:J1');
+                    $event->sheet->getDelegate()->getStyle('A1:J1')->applyFromArray($centrar);
 
-                $event->sheet->getDelegate()->getStyle('A3:F3')->applyFromArray($header);
-                $event->sheet->getDelegate()->getStyle('A3:F3')->getFill()->setFillType(\PhpOffice\PhpSpreadsheet\Style\Fill::FILL_SOLID)->getStartColor()->setARGB('99a3a4');
-                for ($i = 1; $i <= $this->item; $i++) {
-                    $event->sheet->getDelegate()->getStyle('A' . ($i + 2))->applyFromArray($negrita);
-                    $event->sheet->getDelegate()->getStyle('A' . ($i + 2) . ':F' . ($i + 2))->applyFromArray($border);
-                    $event->sheet->getDelegate()->getStyle('A' . ($i + 2) . ':B' . ($i + 2))->applyFromArray($centrar);
-                    $event->sheet->getDelegate()->getStyle('E' . ($i + 2) . ':E' . ($i + 2))->applyFromArray($centrar);
+                    $columnas = ['N°', 'DNI', 'APELLIDOS', 'NOMBRES', 'CELULAR', 'CORREO', 'PUNTAJE EXPEDIENTE', 'PUNTAJE ENTREVISTA', 'PUNTAJE FINAL', 'PROGRAMA ACADEMICO'];
+                    $event->sheet->getDelegate()->fromArray($columnas, NULL, 'A3');
+
+                    $event->sheet->getDelegate()->getStyle('A3:J3')->applyFromArray($header);
+                    $event->sheet->getDelegate()->getStyle('A3:J3')->getFill()->setFillType(\PhpOffice\PhpSpreadsheet\Style\Fill::FILL_SOLID)->getStartColor()->setARGB('99a3a4');
+                    for ($i = 1; $i <= $this->item; $i++) {
+                        $event->sheet->getDelegate()->getStyle('A' . ($i + 2))->applyFromArray($negrita);
+                        $event->sheet->getDelegate()->getStyle('A' . ($i + 2) . ':J' . ($i + 2))->applyFromArray($border);
+                        $event->sheet->getDelegate()->getStyle('A' . ($i + 2) . ':B' . ($i + 2))->applyFromArray($centrar);
+                        $event->sheet->getDelegate()->getStyle('E' . ($i + 2) . ':E' . ($i + 2))->applyFromArray($centrar);
+                        $event->sheet->getDelegate()->getStyle('G' . ($i + 2) . ':I' . ($i + 2))->applyFromArray($centrar);
+                    }
+                } else {
+                    $event->sheet->getDelegate()->getStyle('A1:K1')->applyFromArray($tamanio);
+                    $event->sheet->getDelegate()->getStyle('A1:K1')->applyFromArray($negrita);
+                    $event->sheet->getDelegate()->mergeCells('A1:K1');
+                    $event->sheet->getDelegate()->getStyle('A1:K1')->applyFromArray($centrar);
+
+                    $columnas = ['N°', 'DNI', 'APELLIDOS', 'NOMBRES', 'CELULAR', 'CORREO', 'PUNTAJE EXPEDIENTE', 'PUNTAJE INVESTIGACION', 'PUNTAJE ENTREVISTA', 'PUNTAJE FINAL', 'PROGRAMA ACADEMICO'];
+                    $event->sheet->getDelegate()->fromArray($columnas, NULL, 'A3');
+
+                    $event->sheet->getDelegate()->getStyle('A3:K3')->applyFromArray($header);
+                    $event->sheet->getDelegate()->getStyle('A3:K3')->getFill()->setFillType(\PhpOffice\PhpSpreadsheet\Style\Fill::FILL_SOLID)->getStartColor()->setARGB('99a3a4');
+                    for ($i = 1; $i <= $this->item; $i++) {
+                        $event->sheet->getDelegate()->getStyle('A' . ($i + 2))->applyFromArray($negrita);
+                        $event->sheet->getDelegate()->getStyle('A' . ($i + 2) . ':K' . ($i + 2))->applyFromArray($border);
+                        $event->sheet->getDelegate()->getStyle('A' . ($i + 2) . ':B' . ($i + 2))->applyFromArray($centrar);
+                        $event->sheet->getDelegate()->getStyle('E' . ($i + 2) . ':E' . ($i + 2))->applyFromArray($centrar);
+                        $event->sheet->getDelegate()->getStyle('G' . ($i + 2) . ':J' . ($i + 2))->applyFromArray($centrar);
+                    }
                 }
+
             },
         ];
     }
