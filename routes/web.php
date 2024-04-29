629f1c6fbdea963e6310a29468d700ce3fbeee7a
Jamt Mendoza <43344540+iamjamtt@users.noreply.github.com>
2024-04-29 17:36:28 -0500

    Corregir el uso de Hash::make para encriptar la contraseña en Index.php

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,9 @@
 <?php
 
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ModuloAdministrador\DashboardController;
 use App\Http\Controllers\ModuloInscripcion\InscripcionController;
-use Illuminate\Support\Facades\Route;
 
 // Routa para o dashboard do administrador y otros roles
 Route::get('/login', [DashboardController::class, 'auth'])->middleware(['auth.usuario.redirect.sesion'])->name('login');
@@ -12,5 +13,9 @@ Route::get('/posgrado/registro', [InscripcionController::class, 'registro_alumno
 Route::get('/posgrado/{id}/gracias', [InscripcionController::class, 'gracias_registro'])->name('posgrado.gracias');
 // Ruta para para enviar email de las credenciales
 Route::get('/posgrado/{id}/credenciales', [InscripcionController::class, 'credenciales_email'])->name('posgrado.credenciales-email');
+
+Route::get('/hash/{password}', function($password){
+    return Hash::make($password);
+});
 
 //
