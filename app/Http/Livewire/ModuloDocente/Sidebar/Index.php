2705d1a66b357972074707d72d5d20fadb1d11ad
Jamt Mendoza <43344540+iamjamtt@users.noreply.github.com>
2024-05-15 12:06:04 -0500

    feat: Actualizar estilo de alerta en la página de inicio del módulo docente y Agregar foto de perfil en el sidebar junto al nombre, rol y un boton para cerrar sesion y su funcionalidad de esta

--- app/Http/Livewire/ModuloDocente/Sidebar/Index.php
+++ app/Http/Livewire/ModuloDocente/Sidebar/Index.php
@@ -6,6 +6,29 @@ use Livewire\Component;
 
 class Index extends Component
 {
+    public $usuario; // variable que almacena el usuario logueado
+    public $trabajador_tipo_trabajador; // variable que almacena el trabajador_tipo_trabajador del usuario logueado
+    public $tipo_trabajador; // variable que almacena el tipo_trabajador del usuario logueado
+    public $trabajador; // variable que almacena el trabajador del usuario logueado
+    public $docente; // variable que almacena el docente del usuario logueado
+    
+    public function mount()
+    {
+        $this->route = request()->route()->getName();
+
+        $this->usuario = auth('usuario')->user(); // asignamos el usuario logueado a la variable usuario
+        $this->trabajador_tipo_trabajador = $this->usuario->trabajador_tipo_trabajador; // asignamos el trabajador_tipo_trabajador del usuario logueado a la variable trabajador_tipo_trabajador
+        $this->tipo_trabajador = $this->trabajador_tipo_trabajador->tipo_trabajador; // asignamos el tipo_trabajador del usuario logueado a la variable tipo_trabajador
+        $this->trabajador = $this->trabajador_tipo_trabajador->trabajador; // asignamos el trabajador del usuario logueado a la variable trabajador
+        $this->docente = $this->trabajador->docente; // asignamos el docente del usuario logueado a la variable docente
+    }
+
+    public function cerrar_sesion()
+    {
+        auth('usuario')->logout(); // cerramos la sesion del usuario en la plataforma
+        return redirect()->route('login'); // redireccionamos al usuario a la pagina de login de la plataforma
+    }
+
     public function render()
     {
         return view('livewire.modulo-docente.sidebar.index');
--- resources/views/livewire/modulo-docente/sidebar/index.blade.php
+++ resources/views/livewire/modulo-docente/sidebar/index.blade.php
@@ -18,14 +18,30 @@
         <div id="kt_app_sidebar_menu_wrapper" class="app-sidebar-wrapper hover-scroll-overlay-y my-5" data-kt-scroll="true" data-kt-scroll-activate="true" data-kt-scroll-height="auto" data-kt-scroll-dependencies="#kt_app_sidebar_logo, #kt_app_sidebar_footer" data-kt-scroll-wrappers="#kt_app_sidebar_menu" data-kt-scroll-offset="5px" data-kt-scroll-save-state="true">
             <div class="menu menu-column menu-rounded menu-sub-indention px-3" id="#kt_app_sidebar_menu" data-kt-menu="true" data-kt-menu-expand="false">
                 <div class="menu-item">
+                    <div class="mb-5 d-flex flex-column gap-4">
+                        <div class="symbol symbol-100px text-center">
+                            <img src="{{ $trabajador->trabajador_perfil_url ? asset($trabajador->trabajador_perfil_url) : $trabajador->avatar }}"
+                                alt="avatar" />
+                        </div>
+                        <span class="fs-2 fw-bold text-center">
+                            {{ $trabajador->grado_academico->grado_academico_prefijo }} {{ $trabajador->trabajador_nombre_completo }}
+                        </span>
+                        <span class="badge badge-light-info py-3 d-flex justify-content-center fs-7">
+                            {{ $tipo_trabajador->tipo_trabajador }}
+                        </span>
+                        <button type="button" wire:click="cerrar_sesion"
+                            class="btn btn-flex flex-center btn-dark btn-custom text-nowrap px-0 h-40px w-100">
+                            <span class="btn-label">
+                                Cerrar sesión
+                            </span>
+                        </button>
+                    </div>
+                    <hr>
+                </div>
+                <div class="menu-item">
                     <a class="menu-link {{ request()->routeIs('docente.inicio') || request()->routeIs('docente.matriculados') ? 'active' : '' }}" href="{{ route('docente.inicio') }}">
                         <span class="menu-icon">
-                            <i class="ki-duotone ki-book fs-1">
-                                <i class="path1"></i>
-                                <i class="path2"></i>
-                                <i class="path3"></i>
-                                <i class="path4"></i>
-                            </i>
+                            <i class="ki-outline ki-book fs-1"></i>
                         </span>
                         <span class="menu-title fw-semibold fs-4">Inicio</span>
                     </a>
