b91f679e38a4603918fb2d63d4957f7a9c265fa3
Jamt Mendoza <43344540+iamjamtt@users.noreply.github.com>
2024-05-16 09:11:14 -0500

    feat: Eliminar código comentado y desactivar funcionalidad de ver voucher en la vista de pagos

--- resources/views/livewire/modulo-plataforma/pagos/index.blade.php
+++ resources/views/livewire/modulo-plataforma/pagos/index.blade.php
@@ -110,7 +110,7 @@
                                         <th>Monto</th>
                                         <th>Fecha</th>
                                         <th>Estado</th>
-                                        <th>Voucher</th>
+                                        {{-- <th>Voucher</th> --}}
                                         <th class="text-end">Acciones</th>
                                     </tr>
                                 </thead>
@@ -143,11 +143,11 @@
                                                 <span class="badge badge-danger fs-6 px-3 py-2">Observado</span>
                                             @endif
                                         </td>
-                                        <td>
+                                        {{-- <td>
                                             <a href="{{ asset($item->pago_voucher_url) }}" target="_blank" class="btn btn-outline btn-outline-info">
                                                 Ver Voucher
                                             </a>
-                                        </td>
+                                        </td> --}}
                                         <td class="text-end">
                                             @if ($item->pago_verificacion != 2)
                                                 <a href="#modal_pago_plataforma" wire:click="cargar_pago({{ $item->id_pago }})" class="btn btn-outline btn-outline-dashed btn-outline-primary btn-active-light-primary hover-scale" data-bs-toggle="modal" data-bs-target="#modal_pago_plataforma">
