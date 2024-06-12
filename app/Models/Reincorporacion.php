c31656ddf3414f808a3823a2ba2aeb9272040ecc
Jamt Mendoza <43344540+iamjamtt@users.noreply.github.com>
2024-06-12 18:14:17 -0500

    Actualizar modelo Reincorporacion para incluir relación BelongsTo con el modelo Admitido

--- app/Models/Reincorporacion.php
+++ app/Models/Reincorporacion.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
-class Reincoporacion extends Model
+class Reincorporacion extends Model
 {
     use HasFactory;
 
